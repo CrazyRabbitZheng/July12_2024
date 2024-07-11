33fcea004d86cd3d8eecb90b05cdec9cc336945d
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-11 12:22:33 -0230

    update

---
+++ src/SquareCubeTable.java
@@ -0,0 +1,19 @@
+/**
+ * This class holds a square and cube table for numbers 2 to 9
+ * @author--Zheng Wang Stu.ID: 20085058
+ */
+public class SquareCubeTable {
+    public static void main(String[] args) {
+        System.out.printf("%-10s\t%-10s\t%-10s%n","number","square","cube");
+        System.out.printf("%-10d\t%-10d\t%-10d%n",2,2 * 2, 2 * 2 * 2);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",3,3 * 3, 3 * 3 * 3);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",4,4 * 4, 4 * 4 * 4);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",5,5 * 5, 5 * 5 * 5);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",6,6 * 6, 6 * 6 * 6);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",7,7 * 7, 7 * 7 * 7);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",8,8 * 8, 8 * 8 * 8);
+        System.out.printf("%-10d\t%-10d\t%-10d%n",9,9 * 9, 9 * 9 * 9);
+
+
+    }
+}
