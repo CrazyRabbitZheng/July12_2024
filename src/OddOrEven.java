c931827cde548fe5ec2442693475311ef6d4baf1
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-11 11:10:46 -0230

    update

---
+++ src/OddOrEven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * This class tells an input to be odd or even
+ * @author--Zheng Wang Stu.ID: 20085058
+ */
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        if(num == 1) {
+            System.out.printf("%d is odd", num);
+        }else if (num % 2 == 0) {
+            System.out.printf("%d is even", num);
+        }else {
+            System.out.printf("%d is odd", num);
+        }
+    }
+}
