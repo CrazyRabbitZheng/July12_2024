6d870653abc18680be5a3007bfc935d0991ae4e4
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-11 11:21:01 -0230

    update

---
+++ src/CheckerboardAsterisk.java
@@ -0,0 +1,15 @@
+/**
+ * This class uses asterisks to display a checkerboard
+ */
+public class CheckerboardAsterisk {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+    }
+}
